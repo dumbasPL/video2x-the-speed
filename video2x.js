d82283969cffd56314d10b3926e2ec1b43075f0b
workpc <ja@DESKTOP-370JG9D>
2020-07-13 10:30:39 +0200

    backup

--- video2x.js
+++ video2x.js
@@ -2,6 +2,8 @@
 const yargs = require('yargs');
 const fs = require('fs');
 const path = require('path');
+const commandExists = require('command-exists').sync;
+const child_process = require('child_process');
 
 const argv = yargs
     .option('input', {
@@ -61,17 +63,21 @@ const argv = yargs
         type: "number",
         default: 2
     })
+    .option('verbose', {
+        alias: 'v',
+        description: 'verbose output',
+        type: "boolean",
+    })
     .group([], 'Options:') //forces the default "Options:" group to be on top
     .help(true, "Show this help message")
     .alias('help', 'h')
     .version()
-    .alias('version', 'v')
-    .group(['help', 'version'], "Other")
+    .group(['help', 'version', 'verbose'], "Other")
     .detectLocale(false)
-    .wrap(Math.min(80, yargs.terminalWidth()))
-    .check((argv, options) => {
-        console.debug(argv.i, argv.o);
+    .wrap(Math.min(100, yargs.terminalWidth()))
+    .check((argv) => {
         let files = [];
+        let out_files = [];
         if(typeof argv.i != "string") {
             for (let i = 0; i < argv.i.length; i++) {
                 const element = argv.i[i];
@@ -84,13 +90,57 @@ const argv = yargs
             if(fs.statSync(argv.i).isFile()) {
                 files.push(path.resolve(argv.i));
             } else {
-                let files = fs.readdirSync(path.i);
-                
+                let in_dir_files = fs.readdirSync(argv.i);
+                in_dir_files.forEach(element=> {
+                    let file = path.resolve(path.join(argv.i, element));
+                    if(fs.statSync(file).isFile()) {
+                        files.push(file);
+                    }
+                });
             }
         }
-
-        throw "";
+        if(files.length == 0) throw `No files found in specified input directory`;
+        if(typeof argv.o != "string") throw `Multiple outputs are not supported`;
+        if(!fs.existsSync(argv.o) || fs.statSync(argv.o).isFile()) { //FIXME: bug: same in and out directory
+            if(files.length > 1) throw `Use a single directory as output when using mutiple input files`;
+            else out_files.push(path.resolve(argv.o));
+        } else {
+            let out_dir = path.resolve(argv.o);
+            files.forEach(file => {
+                out_files.push(path.resolve(path.join(out_dir, path.basename(file)))); //FIXME: bug: multiple input files wit the same name
+            });
+        }
+        if(files.length != out_files.length) throw `Internal error "files.length != out_files.length" please submit a issue on github`
+        argv.i = files;
+        argv.o = out_files;
+        return true;
     })
     .argv;
 
-console.log(argv.i.length != undefined);
+var verbose = !!argv.v;
+
+if(verbose) process.stderr.write(`Detected ${argv.i.length} input files\n`);
+
+process.env['PATH'] += path.delimiter + path.resolve(path.join(__dirname, "deps", "ffmpeg", "bin"));
+process.env['PATH'] += path.delimiter + path.resolve(path.join(__dirname, "deps", "waifu2x-ncnn-vulkan"));
+
+var ffmpeg_executable = "ffmpeg";
+var waifu2x_executable = "waifu2x-ncnn-vulkan";
+
+if(!commandExists("ffmpeg")) {
+    if(commandExists("ffmpeg.exe")) {
+        ffmpeg_executable = "ffmpeg.exe"; //for WSL compatibility ;)
+    } else {
+        console.error("could not find ffmpeg");
+        process.exit(-1);
+    }
+}
+if(!commandExists("waifu2x-ncnn-vulkan")) {
+    if(commandExists("waifu2x-ncnn-vulkan.exe")) {
+        waifu2x_executable = "waifu2x-ncnn-vulkan.exe"; //for WSL compatibility ;)
+    } else {
+        console.error("could not find waifu2x-ncnn-vulkan");
+        process.exit(-1);
+    }
+}
+if(verbose) process.stderr.write(`Using "${ffmpeg_executable}" as ffmpeg executable and "${waifu2x_executable}" as waifu2x executable\n`);
